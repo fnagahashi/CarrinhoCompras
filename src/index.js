e5c7a725d9f17727059bb61f086c0e54dd187d92
fnagahashi <fabi.nagahashi@gmail.com>
2025-09-16 20:45:29 -0300

    feat: Add item, deleteItem, subtotal, totalCart

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import createItem from "./services/item.js";
+import * as cartService from "./services/cart.js";
  
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log("Bem-vindo ao carrinho de compras da Shopee");
 
 const item1 = await createItem("Camisa", 25.99, 1)
 const item2 = await createItem("Calça", 50.00, 2)
+const item3 = await createItem("Bermuda", 35.8, 5)
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWishList, item2);
+await cartService.addItem(myCart, item3);
+await cartService.displayCart(myCart);
+
+// await cartService.deleteItem(myCart, item3.name);
+// await cartService.deleteItem(myCart, item1.name);
+
+
+await cartService.totalCart(myCart);
+
+
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,12 +1,15 @@
 
 // CASOS DE USO
 // adicionar item no carrinho
-async function addItem(userCart){
-
+async function addItem(userCart, item){
+    userCart.push(item);
 }
 // deletar item do carrinho
 async function deleteItem(userCart, name){
-
+    const index = userCart.findIndex((item) => item.name === name);
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
 }
 // remover item do carrinho - dimninuir a quantidade
 async function removeItem(userCart, index){
@@ -14,5 +17,17 @@ async function removeItem(userCart, index){
 }
 // calcular o valor total do carrinho
 async function totalCart(userCart){
+    console.log("\nShopee Cart TOTAL:");
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+    console.log(`🛒 Total: R$${result}`);
+}
+
+async function displayCart(userCart){
+    console.log("\nShopee Cart List: ")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}.${item.name} - R$${item.price} | ${item.quantity}x | Subtotal: R$${item.subtotal()}`);
+    })
 
 }
+
+export { addItem, deleteItem, removeItem, totalCart, displayCart };
